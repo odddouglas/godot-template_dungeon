9c98650bf173d5b1dba20efa07a5c65df480404b
odddouglas <18718048671iota@gmail.com>
2024-11-22 15:37:40 +0800

    feat: 修缮脚本animation_controller.gd，完成角色另外斜四个方向的动画控制

--- scripts/animation_controller.gd
+++ scripts/animation_controller.gd
@@ -8,18 +8,31 @@ const WALK_TO_IDLE = {
 	"back_walk": "back_idle",
 	"front_walk": "front_idle",
 	"right_walk": "right_idle",
-	"left_walk": "left_idle"
+	"left_walk": "left_idle",
+	"left_up_walk": "left_idle",
+	"left_down_walk": "left_idle",
+	"right_up_walk": "right_idle",
+	"right_down_walk": "right_idle"
 }
 
 # 播放移动动画，根据速度矢量方向判断当前的移动动画
 func play_walk_animation(velocity: Vector2):
-	if velocity.x > 0: # 向右移动
+	if velocity.x > 0 and velocity.y < 0: # 右上方向
 		play("right_walk")
-	elif velocity.x < 0: # 向左移动
+	elif velocity.x > 0 and velocity.y > 0: # 右下方向
+		play("right_walk")
+	elif velocity.x < 0 and velocity.y < 0: # 左上方向
 		play("left_walk")
-	if velocity.y > 0: # 向下移动
+	elif velocity.x < 0 and velocity.y > 0: # 左下方向
+		play("left_walk")
+		
+	elif velocity.y == 0 and velocity.x > 0: # 向右移动
+		play("right_walk")
+	elif velocity.y == 0 and velocity.x < 0: # 向左移动
+		play("left_walk")
+	elif velocity.x == 0 and velocity.y > 0: # 向下移动
 		play("front_walk")
-	elif velocity.y < 0: # 向上移动
+	elif velocity.x == 0 and velocity.y < 0: # 向上移动
 		play("back_walk")
 
 # 播放静止动画，将当前动画转换为静止状态
