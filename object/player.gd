e8d4b38b936623ac314416a1a36a432794b32aac
odddouglas <18718048671iota@gmail.com>
2024-11-22 15:55:25 +0800

    feat: 修缮脚本player.gd，animation_controller.gd，加入了奔跑动画，实现八向移动控制，后续可以加入斜方向的动画

--- object/player.gd
+++ object/player.gd
@@ -1,28 +1,35 @@
 extends CharacterBody2D
-class_name player
+class_name Player
 
 @onready var animated_sprite_2d: AnimationController = $AnimatedSprite2D # 控制玩家动画的节点
 
-const SPEED = 5000.0
+const SPEED_WALK = 5000.0 # 步行速度
+const SPEED_RUN = 8000.0 # 跑步速度
 
 # 物理帧处理函数
 func _physics_process(delta: float) -> void:
+	# 检测是否按住了 Shift 键
+	var is_running = Input.is_action_pressed("shift")
 	
 	# 获取玩家的输入方向向量
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
 	# 如果有方向输入，设置速度矢量
 	if direction:
-		velocity = direction * SPEED * delta
+		var speed = SPEED_RUN if is_running else SPEED_WALK # 根据是否跑步选择速度
+		velocity = direction * speed * delta
+
+		# 播放对应动画
+		if is_running:
+			animated_sprite_2d.play_run_animation(velocity)
+		else:
+			animated_sprite_2d.play_walk_animation(velocity)
 	else:
 		# 如果没有输入，逐步将速度减为 0（缓动效果）
-		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-		velocity.y = move_toward(velocity.y, 0, SPEED * delta)
+		velocity.x = move_toward(velocity.x, 0, SPEED_WALK * delta)
+		velocity.y = move_toward(velocity.y, 0, SPEED_WALK * delta)
 
-	# 如果速度不为零，播放移动动画；否则播放静止动画
-	if velocity != Vector2.ZERO:
-		animated_sprite_2d.play_walk_animation(velocity)
-	else:
+		# 播放静止动画
 		animated_sprite_2d.play_idle_animation()
 
 	# 移动角色并处理碰撞
--- scripts/animation_controller.gd
+++ scripts/animation_controller.gd
@@ -1,5 +1,5 @@
 extends AnimatedSprite2D
-#继承得到animation这个成员
+# 继承 AnimatedSprite2D，控制动画播放
 
 class_name AnimationController
 
@@ -15,7 +15,18 @@ const WALK_TO_IDLE = {
 	"right_down_walk": "right_idle"
 }
 
-# 播放移动动画，根据速度矢量方向判断当前的移动动画
+const RUN_TO_IDLE = {
+	"back_run": "back_idle",
+	"front_run": "front_idle",
+	"right_run": "right_idle",
+	"left_run": "left_idle",
+	"left_up_run": "left_idle",
+	"left_down_run": "left_idle",
+	"right_up_run": "right_idle",
+	"right_down_run": "right_idle"
+}
+
+# 播放步行移动动画，根据速度矢量方向判断当前的移动动画
 func play_walk_animation(velocity: Vector2):
 	if velocity.x > 0 and velocity.y < 0: # 右上方向
 		play("right_walk")
@@ -35,9 +46,30 @@ func play_walk_animation(velocity: Vector2):
 	elif velocity.x == 0 and velocity.y < 0: # 向上移动
 		play("back_walk")
 
+# 播放跑步移动动画，根据速度矢量方向判断当前的跑步动画
+func play_run_animation(velocity: Vector2):
+	if velocity.x > 0 and velocity.y < 0: # 右上方向
+		play("right_run")
+	elif velocity.x > 0 and velocity.y > 0: # 右下方向
+		play("right_run")
+	elif velocity.x < 0 and velocity.y < 0: # 左上方向
+		play("left_run")
+	elif velocity.x < 0 and velocity.y > 0: # 左下方向
+		play("left_run")
+		
+	elif velocity.y == 0 and velocity.x > 0: # 向右移动
+		play("right_run")
+	elif velocity.y == 0 and velocity.x < 0: # 向左移动
+		play("left_run")
+	elif velocity.x == 0 and velocity.y > 0: # 向下移动
+		play("front_run")
+	elif velocity.x == 0 and velocity.y < 0: # 向上移动
+		play("back_run")
+
 # 播放静止动画，将当前动画转换为静止状态
 func play_idle_animation():
-	# 检查当前动画是否在 MOVEMENT_TO_IDLE 中
+	# 检查当前动画是否在 WALK_TO_IDLE 或 RUN_TO_IDLE 中
 	if WALK_TO_IDLE.keys().has(animation):
-		# 播放对应的静止动画
 		play(WALK_TO_IDLE[animation])
+	elif RUN_TO_IDLE.keys().has(animation):
+		play(RUN_TO_IDLE[animation])
