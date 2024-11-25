360d75e305c7113ed88246bd909cad3ed9e70d99
odddouglas <18718048671iota@gmail.com>
2024-11-25 19:33:20 +0800

    add: 增加dialogue相关脚本，实现高自定义的对话框功能，包括选择以及出发逻辑，文案填充，原画填充

--- scripts/player.gd
+++ scripts/player.gd
@@ -36,6 +36,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
-func _on_area_2d_area_entered(area:Area2D) -> void:
+func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area is Item:
-		area.queue_free()
+		DialogueManager.show_dialogue_balloon(load("res://dialogue/test.dialogue"), "this_is_a_node_title")
+		#area.queue_free()
